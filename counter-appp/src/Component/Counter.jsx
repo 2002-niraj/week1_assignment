ff48c4378b2468b8d72f7dd8641ee3c7345c9c94
Niraj palmur <niraj.palmur@emergys.com>
2025-05-02 18:38:49 +0530

    added new feature in counter app

--- counter-appp/src/Component/Counter.jsx
+++ counter-appp/src/Component/Counter.jsx
@@ -1,36 +1,72 @@
-import { useState } from 'react';
-import './Counter.css';
+import { useState,useEffect } from "react";
+import "./Counter.css";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import TungstenTwoToneIcon from '@mui/icons-material/TungstenTwoTone';
+import DarkModeTwoToneIcon from '@mui/icons-material/DarkModeTwoTone';
 
-export default function Counter({initialValue}) {
+export default function Counter({ initialValue }) {
+  const [count, setCount] = useState(initialValue);
+  const [darkMode, setDarkMode] = useState(false);
 
-    const [count , setCount] = useState(initialValue);
-    const increment = ()=>{
-        setCount(count+1);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const reset = () => {
+    setCount(0);
+  };
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      return;
     }
-    const reset = ()=>{
-        setCount(0);
-    }
-    const decrement = ()=>{
+  };
+ 
+  const toggle = () => {
+    const newMode = !darkMode;
+    setDarkMode(newMode);
+    if (newMode) {
+       
+        document.body.style.backgroundColor = "#2c3e50"; 
+        document.body.style.color = "#f1c40f"; 
+        document.title = "Dark-mode"          
+      } else {
         
-        if(count>0){
-            setCount(count-1);
-        }
-        else{
-            return;
-        }
-    }
+        document.body.style.backgroundColor = "#ffff"; 
+        document.body.style.color = "#0000";    
+        document.title = "Light-mode"           
+      }
+
+  };
+
   return (
     <div>
-          <h1>Counter App</h1>
-          <div  >
-              <h2>Counter value: {count}</h2>
+        <div className="heading">
+        <h1 style={{ color: darkMode ? "#fff" : "#333" }} >Counter App</h1>
+      </div>
+   
+      <div className="counter-container">
+        <div>
+          <h2>Counter value: {count}</h2>
 
-              <div className='btn-group' >
-              <button className='btn' onClick={increment} >Increment</button> 
-              <button className='btn' onClick={reset} >Reset</button> 
-              <button className='btn' onClick={decrement} >Decrement</button>
-              </div>
+          <div className="btn-group">
+            <button className="btn increment-btn" onClick={increment}>
+             <AddIcon/>
+            </button>
+            <button className="btn reset-btn" onClick={reset}>
+             <RestartAltIcon/>
+            </button>
+            <button className="btn decrement-btn" onClick={decrement}>
+            <RemoveIcon/>
+            </button>
+            <button className="btn toggle-btn" style={darkMode ? { backgroundColor: "#2c3e50" } : {}} onClick={toggle} >
+            {darkMode ? <DarkModeTwoToneIcon /> : <TungstenTwoToneIcon />}
+            </button>
           </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
